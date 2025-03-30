456dce1be7577dd55c51e8e79571c709d1c405ff
Camila Poloche Londoño <camila.polochel@autonoma.edu.co>
2025-03-29 20:11:57 -0500

    Creacion paquetes

---
+++ src/autonoma/simulacionVehiculo/main/SimulacionVehiculo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.simulacionVehiculo.main;
+
+/**
+ * main
+ * @author Camila
+ * @since 20250329
+ * @version 1.0
+ */
+public class SimulacionVehiculo {
+    public static void main(String[] args) {
+        
+    }
+}
