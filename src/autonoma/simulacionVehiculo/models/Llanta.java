3793d7f788bf782de792557bf960d00c330c963f
Camila Poloche Londoño <camila.polochel@autonoma.edu.co>
2025-04-04 13:33:01 -0500

    Añadida clase Vehiculo y Llanta

--- src/autonoma/simulacionVehiculo/models/Llanta.java
+++ src/autonoma/simulacionVehiculo/models/Llanta.java
@@ -3,11 +3,62 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package autonoma.simulacionVehiculo.models;
-
 /**
- *
+ * Modelo que permite representar una Llanta
  * @author Camila
+ * @since 20250404
+ * @version 1.0
  */
 public class Llanta {
-    
+    //Atributos
+    /**
+    * Tipo llanta
+    */
+    private String tipoLlanta;
+    /**
+    * Limite de velocidad permitido
+    */
+    private String limiteVelocidadPermitido;
+
+    /**
+     * Inicializa los atributos de la clase Boleta
+     * @param tipoLlanta
+     * @param limiteVelocidadPermitido
+    */
+    public Llanta(String tipoLlanta, String limiteVelocidadPermitido) {
+        this.tipoLlanta = tipoLlanta;
+        this.limiteVelocidadPermitido = limiteVelocidadPermitido;
+    }
+
+    /**
+     * Retornar el tipo de llanta
+     * @return tipoLlanta
+    */
+    public String getTipoLlanta() {
+        return tipoLlanta;
+    }
+
+    /**
+     * Modifica el tipo de llanta
+     * @param tipoLlanta
+    */
+    public void setTipoLlanta(String tipoLlanta) {
+        this.tipoLlanta = tipoLlanta;
+    }
+
+    /**
+     * Retornar el limite permitido
+     * @return limitePermitido
+    */
+    public String getLimiteVelocidadPermitido() {
+        return limiteVelocidadPermitido;
+    }
+
+    /**
+     * Modifica el limite permitido
+     * @param limiteVelocidadPermitido
+    */
+    public void setLimitePermitido(String limiteVelocidadPermitido) {
+        this.limiteVelocidadPermitido = limiteVelocidadPermitido;
+    } 
 }
--- src/autonoma/simulacionVehiculo/models/Vehiculo.java
+++ src/autonoma/simulacionVehiculo/models/Vehiculo.java
@@ -3,11 +3,99 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package autonoma.simulacionVehiculo.models;
-
 /**
- *
+ * Modelo que permite representar un Vehiculo
  * @author Camila
+ * @since 20250402
+ * @version 1.0
  */
 public class Vehiculo {
+    //Atributos
+    /**
+    * velocidadActual
+    */
+    private float velocidadActual;
+    /**
+    * estado del vehiculo
+    */
+    private boolean estado = false;
+    /**
+    * Motor
+    */
+    private Motor motor;
+    /**
+    * Llantas del vehiculo
+    */
+    private Llanta llantas;
     
+    /**
+     * Inicializa los atributos de la clase Boleta
+     * @param velocidadActual
+     * @param motor
+     * @param llantas
+    */
+    public Vehiculo(float velocidadActual, Motor motor, Llanta llantas) {
+        this.velocidadActual = velocidadActual;
+        this.motor = motor;
+        this.llantas = llantas;
+    }
+    
+    /**
+     * Enciende el vehiculo
+     * @return String
+    */  
+    public String encender (){
+        this.estado = true;
+        return "Se ha encendido el vehiculo";
+    }
+    
+    /**
+     * Apaga el vehiculo
+     * @return String
+    */  
+    public String apagar (){
+        this.estado = false;
+        return "Se ha apagado el vehiculo";
+    }
+    
+    public boolean verificarBrusquedad (float cambioVelocidad){
+        if (cambioVelocidad > 30){
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Acelera el vehiculo
+     * @param aceleracion
+     * @return String
+    */  
+    public String acelerar (float aceleracion){
+        if (verificarBrusquedad(aceleracion)){
+            
+        }
+        this.velocidadActual += aceleracion;
+        return "Ha acelerado el vehiculo";
+    }
+    
+    /**
+     * Frena el vehiculo
+     * @param frenado
+     * @return String
+    */  
+    public String frenar (float frenado){
+        if (verificarBrusquedad(frenado)){
+            
+        }
+        this.velocidadActual -= frenado;
+        return "Ha frenado el vehiculo";
+    }
+    
+    /**
+     * Frena bruscamente el vehiculo
+     * @return String
+    */  
+    public String frenarBruscamente (){
+        return "";
+    }
 }
