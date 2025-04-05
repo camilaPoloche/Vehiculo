56f26d99b4078a6e2fb238dc4e0f0100cdbd148a
marianaRivas02 <rivasclavijom12@gmail.com>
2025-04-05 09:19:48 -0500

    pruebas

--- src/autonoma/simulacionVehiculo/views/VentanaPrincipal.java
+++ src/autonoma/simulacionVehiculo/views/VentanaPrincipal.java
@@ -32,11 +32,11 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 632, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 454, Short.MAX_VALUE)
         );
 
         pack();
